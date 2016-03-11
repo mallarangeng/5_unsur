8c6db700f8159f61ae86af7ec6262e0039f3d89c
hendrimamang <hendrimamang@gmail.com>
2016-03-12 02:48:57 +0700

    Commit form Login

---
+++ login.php
@@ -0,0 +1,82 @@
+<html>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>PPG Tangerang Barat</title>
+<link id="favicon" rel="shortcut icon" href="images/favicon/favicon.png" />
+<link href="bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css" />
+<link href="bootstrap/source-sans-pro/source-sans-pro.css" rel="stylesheet" type="text/css" />
+  <link href="bootstrap/css/custom.css" rel="stylesheet" type="text/css" />
+    <script src="bootstrap/js/jquery-1.10.2.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <script type="text/javascript"></script>
+  	<link href="jquery/jquery-ui.css" rel="stylesheet" type="text/css" />  
+	<script src="jquery/jquery-ui.js"></script>
+ 	<link href="images/logo.png" rel="shortcut icon" />
+</head>
+
+
+<body>
+    <div class="navbar navbar-default navbar-fixed-top">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a href="index.php" class="navbar-brand"><font class="merah">LOGIN LIMA UNSUR</font></a>
+          <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </div>
+        <div class="navbar-collapse collapse" id="navbar-main">
+          <ul class="nav navbar-nav">
+       
+          </ul>
+
+        </div>
+      </div>
+    </div>
+      <!--BODY
+      !-->
+    
+    <div class="container-fluid">
+      <br>
+<p>
+<br>
+<p>
+  <h3></h3>
+
+
+        <form role="form" class="form-horizontal col-md-4">
+  <div class="form-group">
+    <label for="email">ID DESA / KELOMPOK</label>
+    <input type="email" class="form-control" id="email">
+  </div>
+  <div class="form-group">
+    <label for="pwd">PASSWORD</label>
+    <input type="password" class="form-control" id="pwd"><br>
+    <button type="submit" class="btn btn-info">Masuk</button>
+  </div>
+
+  
+</form>
+
+  </div>
+
+      <footer>
+      <div class="container-fluid">
+        <hr>
+        <div class="row">
+          <div class="col-lg-12">
+                 <strong><span class="navy">PPG Support</span></strong><br/>
+                    Jika Mengalami masalah dan kendala silahkan hubungi kami
+                    WhastApp / Telp&nbsp;<strong><abbr title="Phone">0857</abbr> 1588 7704</strong>
+                </address>
+
+          </div>
+        </div>
+
+      </footer> 
+    </div>
+
+
+</body>
+</html>
