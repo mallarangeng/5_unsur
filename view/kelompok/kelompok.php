d39264040f7793acbfe022fb3d539d75b90658e8
hendrimamang <hendrimamang@gmail.com>
2016-03-12 04:25:56 +0700

    Penambahan data kelompok dan data laporan

---
+++ view/kelompok/kelompok.php
@@ -0,0 +1,29 @@
+ <table class="table table-hover">
+    <thead>
+      <tr>
+        <th>No</th>
+        <th>Id</th>
+        <th>Nama Kelompok</th>
+        <th>Parent</th>
+        <th>Alamat</th>
+        <th>No Hp</th>
+        <th>Penjab</th>
+        <th>Level</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>John</td>
+        <td>John</td>
+        <td>John</td>
+        <td>John</td>
+        <td>John</td>
+        <td>John</td>
+        <td>Doe</td>
+        <td>john@example.com</td>
+      </tr>
+
+
+    </tbody>
+  </table>
+ <a class="btn btn-info btn-xs" href="?r=kelompok&pg=kelompok_form" role="button">Tambah Data</a>
---
+++ view/kelompok/kelompok_form.php
@@ -0,0 +1,30 @@
+ <form role="form" class="form-horizontal col-md-4">
+  <div class="form-group">
+    <label>Nama Kelompok</label>
+    <input type="nm_kelompok" class="form-control">
+  </div>
+    <div class="form-group">
+    <label>Parent / Desa / Daerah</label>
+    <select class="form-control">
+	  <option>Pilih Parent</option>
+	  <option>Desa</option>
+	  <option>Daerah</option>
+	</select>
+  </div>
+  <div class="form-group">
+    <label>Alamat</label>
+    <input type="alamat" class="form-control">
+  </div>
+  <div class="form-group">
+    <label>Nomor Handphone</label>
+    <input type="alamat" class="form-control">
+  </div>
+  <div class="form-group">
+    <label>Penjab KBM Desa/Kelompok</label>
+    <input type="alamat" class="form-control">
+  </div>
+    
+  <div class="form-group">
+    <button type="submit" class="btn btn-info">Simpan</button>
+  </div>
+</form>
