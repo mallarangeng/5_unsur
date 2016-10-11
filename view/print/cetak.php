3088627c429030b2ed3216338ff9b29a1994869b
mallarangeng <hendrimamang@gmail.com>
2016-10-11 11:31:43 +0700

    Menambahkan fitur print laporan

---
+++ view/print/cetak.php
@@ -0,0 +1,109 @@
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=2.0">
+    <link rel="stylesheet" type="text/css" href="../../bootstrap/css/bootstrap.css">
+    <title>LAPORAN MUSYAWARAH</title>
+</head>
+<body>
+  <div class="container-fluid">
+<font align="center"><h3>HASIL PERTEMUAN LIMA UNSUR</h3></font>
+<style type="text/css">
+td {
+    height: 30px;
+}
+</style>
+<table width="372">
+    <thead>
+      <tr>
+        <th width="158"></th>
+        <th width="19"></th>
+        <th width="179"></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td><strong>Nama Kelompok</strong></td>
+        <td>: </td>
+        <td>Pondok Makmur</td>
+      </tr>
+       <tr>
+        <td><strong>Desa</strong></td>
+        <td>: </td>
+        <td>Pondok Alam Permai</td>
+      </tr>
+       <tr>
+        <td><strong>Bulan</strong></td>
+        <td>: </td>
+        <td>Oktober 2016</td>
+      </tr>
+
+    </tbody>
+  </table>
+<hr>
+ <table class="table table-bordered">
+    <thead>
+      <tr>
+        <th>No</th>
+        <th>Kendala</th>
+        <th>Solusi Alternatif</th>
+      </tr>
+    </thead>
+    <tbody>
+
+      <tr>
+        <td>1</td>
+        <td>Bagaimana jika diadakan pengajian shift bagi yang terkena kerja shift .. ?</td>
+        <td>Diadakan setiap Hari Selasa Jam 9 Pagi</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>Bagaimana jika diadakan pengajian shift bagi yang terkena kerja shift .. ?</td>
+        <td>Diadakan setiap Hari Selasa Jam 9 Pagi</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>Bagaimana jika diadakan pengajian shift bagi yang terkena kerja shift .. ?</td>
+        <td>Diadakan setiap Hari Selasa Jam 9 Pagi</td>
+      </tr>
+      <tr>
+        <td>4</td>
+        <td>Bagaimana jika diadakan pengajian shift bagi yang terkena kerja shift .. ?</td>
+        <td>Diadakan setiap Hari Selasa Jam 9 Pagi</td>
+      </tr>
+      <tr>
+        <td>5</td>
+        <td>Bagaimana jika diadakan pengajian shift bagi yang terkena kerja shift .. ?</td>
+        <td>Diadakan setiap Hari Selasa Jam 9 Pagi</td>
+      </tr>
+
+    </tbody>
+</table>
+<table width="100%">
+    <thead>
+      <tr>
+        <th width="19"></th>
+        <th width="179"></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Mengetahui Pengurus Desa, </td>
+        <td>Tangerang, 02 Oketober 2016</td>
+      </tr>
+       <tr>
+        <td height="42">&nbsp;</td>
+        <td>&nbsp;</td>
+      </tr>
+       <tr>
+        <td>KBM Desa</td>
+        <td>KBM Kelompok</td>
+      </tr>
+
+    </tbody>
+  </table>
+<hr>
+</div>
+</body>
+</html>
+
+ 
